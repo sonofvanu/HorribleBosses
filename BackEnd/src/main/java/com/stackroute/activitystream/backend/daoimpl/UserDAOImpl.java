f687d1eb532c1fd17eee827e200b001233497a5b
sonofvanu <yashyajnesh@gmail.com>
2018-01-11 15:00:34 +0530

    after adding cross origin

--- BackEnd/src/main/java/com/stackroute/activitystream/backend/daoimpl/UserDAOImpl.java
+++ BackEnd/src/main/java/com/stackroute/activitystream/backend/daoimpl/UserDAOImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
